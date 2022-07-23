fa243944956b1b3022641ef0593b5f013f919bd7
hasanbavaqar <bavaqarhasan@gmail.com>
2022-07-24 00:31:48 +0430

    filter product with react

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Categories from './Categories';
+import Menu from './Menu';
+import items from './data';
+
+
+const allCategories = ['همه', ...new Set(items.map((item) => item.category))];
+
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories , setCategories] = useState(allCategories)
+  
+  const filterItems = (category) => {
+    if(category === "همه"){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems)
+  }
+ 
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>محصولات ری اکت</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
